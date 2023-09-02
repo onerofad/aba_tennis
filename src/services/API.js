ce2b2c2a2f2903dc3aba3276f9026a70ff4eadbd
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-02 16:52:44 +0100

    first commit

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function getsignupDetails(){
     return(
     axios.create({
-        baseURL: "http://localhost:8000/api/signup/",
+        baseURL: "https://tennis-backend.vercel.app/api/signup/",
         headers: {
             "Accept": 'application/json',
             "Content-Type": 'application/json'

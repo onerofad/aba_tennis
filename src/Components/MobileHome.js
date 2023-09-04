a36564c70660b4690e834d09132009f4dd25b0d0
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-04 02:07:01 +0100

    first commit

--- src/Components/MobileHome.js
+++ src/Components/MobileHome.js
@@ -213,7 +213,68 @@ const MobileHome = () => {
             </Grid.Row>
         </Grid>
 
-    </Segment>            
+    </Segment>      
+    <Segment style={{padding: '4em 6em'}}>
+        <Grid>
+            <Grid.Row>
+                <Grid.Column width={6}>
+                    <Image src="/images/about/about.png" />
+                </Grid.Column>
+                <Grid.Column width={10}  verticalAlign="middle">
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column>
+                            <p
+                                style={{
+                                    fontFamily: "Poppins",
+                                    color: '#000000',
+                                    fontWeight: 'normal',
+                                    fontSize: '20px',
+                                    lineHeight: '2em',
+                                }}
+                           
+                           >
+                            The 2023 Aba Premier League Table Tennis is a knock-out based open tournament. Its goal is to develop a professional table tennis league in Aba by promoting table tennis skills, professionalism among table tennis players and officials and developing standard playing infrastructure. Players will be able to earn ranking points, get ranked based on their points earned and win performance-based prize money
+
+                            </p>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={6}>
+                            <Button 
+                                style={{
+                                    fontFamily: "Poppins",
+                                    color: '#ffffff',
+                                    fontWeight: 'normal',
+                                    fontSize: '18px',
+                                    backgroundColor: '#193275',
+                                }}
+                            >
+                                Register Now
+                            </Button>
+                            </Grid.Column>
+                            <Grid.Column width={7}>
+                            <Button 
+                                size="huge"
+                                style={{
+                                    fontFamily: "Poppins",
+                                    color: '#193275',
+                                    fontWeight: 'normal',
+                                    fontSize: '18px',
+                                    backgroundColor: '#ffffff',
+                                    border: '1px solid #193275'
+                                }}
+                            
+                            >
+                                Check Prospectus
+                            </Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    </Segment>      
             
         </Sidebar.Pushable>
         </div>

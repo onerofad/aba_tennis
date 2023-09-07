e4a6118102f8ecf604ef686d44160d9d0e96a9e4
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-07 21:43:21 +0100

    first commit

--- src/Components/Db.js
+++ src/Components/Db.js
@@ -4,39 +4,39 @@ export default function getNews() {
     const latestNews = [
         {
             id: '1',
-            description: 'Premier League Table Tennis set to launch in ABA',
+            title: 'FROM THE LEAGUE PRESIDENT',
+            description: 'Aba and its people are renowned for their innovation and doggedness. So, it’s no surprise that out of Aba has come the greatest innovation in professional sports in Nigeria, the Aba Premier League Table Tennis. We are convinced that there is no city in Nigeria today that is equipped like Aba to promote the development of professional sports as an industry for the benefit of all stake holders. Here are a few reasons to support this view. Aba is relatively equidistance from several cities and state capitals, which ensures the mobility and supply of sports fans, a pool of talented athletes and other support professionals. Also, Aba’s Ariaria Market has proven capacity to supply indigenously produced sports shoes, sports clothing and sports equipment required for a sports economy to thrive. As labor always migrates to the place of highest reward, athletes flowing into Aba seeking rewards for their talent will be pleasantly surprised at Aba’s modern hospitality infrastructure of hotels, restaurants, and people ready to welcome them. The question probably on everyone’s mind is how and why should it be table tennis leading this dawn of professional sports and the development of the sports business economy in Nigeria? And our answer is why not? Table tennis has for long been one of the best administratively organised sports in Nigeria, it is participatory, resident in many homes, individualistic and has players spread across a much wider age range than many other sports.But can we execute on our promise of a professional table tennis league in Aba? Afterall this has never been done before? We believe that we have put in place the foundation to achieve this vision including physical and digital infrastructure, prize money, top of the range equipment, merchandise, and broadcast equipment. Ultimately our ability to fully execute to plan is dependent on the support of every stakeholder from the players to the fans, officials, regulators, broadcast partners, vendors, sponsors and of course our host city, Aba. This is a huge project and rightly so too. Sports is growing everywhere in the world and providing employment and reward to many. There is no reason why any athlete, sport, or city in Nigeria, shouldn’t be part of this growth. Our athletes are perhaps more gifted than many of today’s world champions. All that they require is the opportunity and reward for the display of their competitive talents and the entertainment that it provides to fans and viewers. Our ambition is to provide them with both the opportunity and reward beginning with table tennis and then on to other sports. Let us begin.',
             image: '/images/fayose.jpg'
         },
         {
             id: '2',
-            description: 'Players prepare for professional table tennis league',
+            title: 'ITTF CERTIFIED COACH IFEOMA ISAAC TO TRAIN OFFICIALS ',
+            description: 'As part of programs to ensure top quality officiating at the Aba Premier League Table Tennis, a training clinic for tournament umpires and referees is scheduled to hold just before the tournament begins. The clinic will be facilitated by Nigeria’s foremost female ITTF (International Table Tennis Federation) certified table tennis coach, Ifeoma Stella Isaac. According to Coach Ifeoma, “it is important that we introduce our players to the highest global standards of officiating so that they are ready to take on the world when they step out”. She emphasized that standard officiating plays a crucial role in making the Aba Premier League an international tournament within the shortest possible time. She continued by saying, “it is important that we facilitate the understanding of the game by the spectators as well, and one way of ensuring this, is through the provision of international standard officiating so that there are no misinterpretations of how points are won or lost, the conduct of players and the quality of equipment to be used”. Attending the training clinic is mandatory for any person wishing to serve as an umpire or referee during the tournament. Coach Ifeoma is also the tournament director of the Aba Premier League Table Tennis.',
             image: '/images/fayose.jpg'
 
         },
         {
             id: '3',
-            description: 'Umpires and officials to play a crutial role in ABA premier league table tennis',
+            title: 'UNDERSTANDING THE ABA PREMIER LEAGUE TABLE TENNIS',
+            description: 'The Aba Premier League Table Tennis is more than just a League. It also serves as the mother brand for all the table tennis competitions that make up the Aba table tennis season. What this means is that it is both a league and a competition event circuit. Several other table tennis tournaments will be organised under the umbrella of the Aba Premier League Table Tennis. However only the League will have a league table. All other tournaments will only enable players win ranking points.',
             image: '/images/fayose.jpg'
 
         },
         {
             id: '4',
-            description: 'Abia state table tennis association excited about ABA premier league table tennis',
+            title: 'LYSI PRODUCES ABA PREMIER LEAGUE TABLE TENNIS CLOTHING MERCHANDISE AND WEBSITE',
+            description: 'Leading digital marketing and graphic design firm Lysi, had been contracted to deliver the clothing merchandise and website for the Aba Premier League Table Tennis. As part of this delivery, Lysi will also be responsible for managing the League’s YouTube channel and nominated social media handles. In a statement, Lysi expressed their joy at having been awarded the contract and promised to ensure flow of creativity and efficiency across all the league’s brand assets entrusted to them. Part of their statement reads, “this is a unique opportunity for us to help establish a young sporting business franchise and we are pleased to be a key part of this project.” ',
             image: '/images/fayose.jpg'
 
         },
         {
             id: '5',
-            description: 'Watch up comming events from ABA Tennis premier league',
+            title: 'ABA PREMIER LEAGUE TO LAUNCH A TABLE TENNIS SCHOLARSHIP PROGRAM FOR PRIMARY SCHOOLS',
+            description: 'A major goal of the Aba Premier League Table Tennis is its plan to establish a solid value chain of future table tennis players who will support the League and has concluded plans to establish a table tennis scholarship program for schools in Aba. The program which is called Program for Table Tennis Excellence (PTTE) will see the League extending invitation to heads of secondary schools in Aba to inform them about the scheme. The scholarship will be provided at two levels, school, and student. At the school level the winning school will be endowed with table tennis equipment to be placed in a hall and a cash award to promote the play of table tennis in the school. The winning student will also be provided with tuition scholarship for a whole session and will be obliged to attend table tennis camps and clinics organised by PTTE. The winning school and student will be determined from an inter school table tennis tournament organised by the Aba Premier League Table Tennis. ',
             image: '/images/fayose.jpg'
 
         },
-        {
-            id: '6',
-            description: 'Watch Table tennis at its best from ABA premier league',
-            image: '/images/fayose.jpg'
-
-        },
+      
     ]
 
     return latestNews
